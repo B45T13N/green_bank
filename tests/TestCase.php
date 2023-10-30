bf901b49b97c3dff3413399ac766f655f89f8df6
B45T13N <bastds.dev@gmail.com>
2023-10-30 23:11:54 +0100

    Setup .env to test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,15 @@
 
 namespace Tests;
 
+use Dotenv\Dotenv;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    public static function setUpBeforeClass(): void
+    {
+        $dotenv = Dotenv::createImmutable(__DIR__.'/../', '.env.testing');
+        $dotenv->load();
+    }
 }
