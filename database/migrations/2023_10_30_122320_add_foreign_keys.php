fe546e4fee97b119d6f327e24e1418ccb06c4df4
B45T13N <bastds.dev@gmail.com>
2023-10-30 14:45:30 +0100

    Create migrations to setup the db

---
+++ database/migrations/2023_10_30_122320_add_foreign_keys.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vehicle_types', function (Blueprint $table)
+        {
+            $table->bigInteger('notation_id')->unsigned();
+
+            $table->foreign('notation_id')->references('id')->on('gradings');
+        });
+
+        Schema::table('energies', function (Blueprint $table)
+        {
+            $table->bigInteger('notation_id')->unsigned();
+
+            $table->foreign('notation_id')->references('id')->on('gradings');
+        });
+
+        Schema::table('mileages', function (Blueprint $table)
+        {
+            $table->bigInteger('notation_id')->unsigned();
+
+            $table->foreign('notation_id')->references('id')->on('gradings');
+        });
+
+        Schema::table('years', function (Blueprint $table)
+        {
+            $table->bigInteger('notation_id')->unsigned();
+
+            $table->foreign('notation_id')->references('id')->on('gradings');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('vehicle_types', function (Blueprint $table) {
+            $table->dropForeign(['notation_id']);
+            $table->dropColumn('notation_id');
+        });
+
+        Schema::table('energies', function (Blueprint $table) {
+            $table->dropForeign(['notation_id']);
+            $table->dropColumn('notation_id');
+        });
+
+        Schema::table('mileage', function (Blueprint $table) {
+            $table->dropForeign(['notation_id']);
+            $table->dropColumn('notation_id');
+        });
+
+        Schema::table('years', function (Blueprint $table) {
+            $table->dropForeign(['notation_id']);
+            $table->dropColumn('notation_id');
+        });
+    }
+};
---
+++ database/migrations/2023_10_30_122928_create_borrowing_rate.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('borrowing_rates', function (Blueprint $table) {
+            $table->id();
+            $table->integer('score')->nullable(false);
+            $table->integer('rate')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('borrowing_rates');
+    }
+};
