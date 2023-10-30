8bf754906cb177cd32d896ece655ca9b3e9e4c1a
B45T13N <bastds.dev@gmail.com>
2023-10-30 22:14:04 +0100

    Setup the transition

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,6 +1,33 @@
 <template>
-    <h1>
-        How To Install Vue 3 in Laravel 10 : Laravel SPA :)
-    </h1>
-    <router-view />
+    <v-app>
+        <HeaderComponent />
+        <transition name="slide-x" mode="out-in">
+            <router-view class="h-100 w-100 d-flex justify-center align-center"></router-view>
+        </transition>
+    </v-app>
 </template>
+<script setup>
+    import HeaderComponent from './components/Header.vue'
+</script>
+<style lang="scss">
+    @import "../scss/app.scss";
+
+    .slide-x-enter-active,
+    .slide-x-leave-active {
+        transition: transform .6s cubic-bezier(0.25, 0.8, 0.5, 1), opacity .8s;
+        opacity: 1;
+    }
+
+    .slide-x-enter-from,
+    .slide-x-leave-to {
+        opacity: 0;
+    }
+
+    .slide-x-enter-from {
+        transform: translateX(100px);
+    }
+
+    .slide-x-leave-to {
+        transform: translateX(-100px);
+    }
+</style>
