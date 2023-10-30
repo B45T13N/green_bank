df6c2a2c54306af912f377eb811e1ac1c916bf9e
B45T13N <bastds.dev@gmail.com>
2023-10-30 22:14:22 +0100

    Add vuetify and setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,21 @@
 import './bootstrap';
-import '../css/app.css';
-
 import {createApp} from 'vue'
-
 import AppComponent from './App.vue'
 import router from "./router/index.js";
 
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 const app = createApp(AppComponent)
 
+app.use(vuetify);
 app.use(router);
 app.mount("#app")
