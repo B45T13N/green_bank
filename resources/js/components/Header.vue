76646b3ec721047a427e3e0baa2fe0cbdf0d9b57
B45T13N <bastds.dev@gmail.com>
2023-10-30 22:14:47 +0100

    Create header component

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-app-bar>
+        <v-app-bar-title>Green Bank Simulator</v-app-bar-title>
+    </v-app-bar>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
